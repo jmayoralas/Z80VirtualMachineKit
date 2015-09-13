e1c9f11d7b585a99338d898bb1e801a140c65d08
Jose Luis Fernandez-Mayoralas <yo@jluis.me>
2015-09-13 08:50:15 +0200

    Clase para la gestión de memoria terminada, con algunos mensajes de debug que habrá que quitar

--- Cpu/memory.swift
+++ Cpu/memory.swift
@@ -8,11 +8,60 @@
 
 import Foundation
 
-class Memory {
-    var address: UInt16 = 0
-    var data = [UInt8](count: 0x10000, repeatedValue: 0)
-    
-    init() {
+enum MemoryErrors: ErrorType {
+    case MemoryNotConnected
+    case UnknownError
+}
 
+class Memory {
+    let pins: Pins!
+    
+    private var data: [UInt8] = Array(count: 0x10000, repeatedValue: 0)
+    private let rom_ranges: [(start: Int, end: Int)] = [
+        (0x2000, 0x3FFF)
+    ]
+    
+    init(pins: Pins) {
+        self.pins = pins
+    }
+    
+    func clk() {
+        let address = Int(pins.address_bus)
+        
+        // read from memory
+        if pins.rd {
+            print("Memory Read !")
+            pins.data_bus = peek(address)
+        }
+        
+        // write to memory
+        if pins.wr {
+            print("Memory Write!")
+            poke(address, bytes: [pins.data_bus])
+        }
+    }
+    
+    private func isAddressReadOnly(address: Int) -> Bool {
+        for range in rom_ranges {
+            if range.start <= address && address <= range.end {
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func poke(address: Int, bytes: [UInt8]) {
+        var my_address = address
+        if !isAddressReadOnly(address) {
+            for byte in bytes {
+                data[my_address++] = byte
+            }
+            
+        }
+    }
+    
+    func peek(address: Int) -> UInt8 {
+        return data[address]
     }
 }
