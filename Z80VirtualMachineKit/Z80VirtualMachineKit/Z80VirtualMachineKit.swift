51a9c8aeaf741369a0fafac4e431693248a7d1ca
Jose Luis Fernandez-Mayoralas <yo@jluis.me>
2015-12-31 17:01:32 +0100

    Ficheros de proyecto del nuevo framework Z80VirtualMachineKit, que será el que usaré en la futura app del emulador

---
+++ Z80VirtualMachineKit/Z80VirtualMachineKit/Z80VirtualMachineKit.swift
@@ -0,0 +1,45 @@
+//
+//  VirtualMachine.swift
+//  z80
+//
+//  Created by Jose Luis Fernandez-Mayoralas on 31/12/15.
+//  Copyright © 2015 lomocorp. All rights reserved.
+//
+
+import Foundation
+
+public class Z80VirtualMachineKit
+{
+    private let memory : Memory
+    private let cpu : Z80
+    private var io_devices : [IODevice]
+    
+    init() {
+        cpu = Z80()
+        memory = Memory(pins: cpu.pins)
+        io_devices = []
+    }
+    
+    public func step() {
+        cpu.clk()
+        if cpu.pins.mreq {
+            memory.clk() // memory's clock line is connected to mreq pin of cpu
+        }
+        
+        for io_device in io_devices {
+            io_device.clk()
+        }
+    }
+    
+    public func addIoDevice(io_device: IODevice) {
+        io_devices.append(io_device)
+    }
+    
+    public func loadRamAtAddress(address: Int, data: [UInt8]) {
+        memory.poke(address, bytes: data)
+    }
+    
+    public func loadRomAtAddress(address: Int, data: [UInt8]) throws {
+        try memory.loadRomAtAddress(address, data: data)
+    }
+}
