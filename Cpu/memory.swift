53f81d609e3f52c975473dcd6a8e3c68929a642d
Jose Luis Fernandez-Mayoralas <yo@jluis.me>
2015-12-31 16:30:10 +0100

    Agregado gestión de límites de direcciones de memoria y algunos métodos públicos para cargar roms en áreas de la memoria especificada

--- Cpu/memory.swift
+++ Cpu/memory.swift
@@ -9,20 +9,21 @@
 import Foundation
 
 enum MemoryErrors: ErrorType {
+    case AddressOutOfRange
     case MemoryNotConnected
     case UnknownError
 }
 
 class Memory {
+    typealias RomRanges = [(start: Int, end: Int)]
     let pins: Pins!
     
     private var data: [UInt8] = Array(count: 0x10000, repeatedValue: 0)
-    private let rom_ranges: [(start: Int, end: Int)] = [
-        (0x2000, 0x3FFF)
-    ]
+    private var rom_ranges: RomRanges
     
     init(pins: Pins) {
         self.pins = pins
+        self.rom_ranges = []
     }
     
     func clk() {
@@ -30,15 +31,52 @@ class Memory {
         
         // read from memory
         if pins.rd {
-            // print("(MR)")
             pins.data_bus = peek(address)
         }
         
         // write to memory
         if pins.wr {
-            // print("(MW)")
             poke(address, bytes: [pins.data_bus])
         }
+    }
+    
+    func getSize() -> Int {
+        return data.count
+    }
+    
+    func poke(address: Int, bytes: [UInt8]) {
+        if 0 <= address && address + bytes.count - 1 < data.count {
+            var my_address = address
+            if !isAddressReadOnly(address) {
+                for byte in bytes {
+                    data[my_address++] = byte
+                }
+                
+            }
+        }
+    }
+    
+    func peek(address: Int) -> UInt8 {
+        if address >= 0 && address < data.count {
+            return data[address]
+        }
+        
+        return 0
+    }
+    
+    func loadRomAtAddress(address: Int, data: [UInt8]) throws {
+        if address < 0 || address + data.count - 1 >= self.data.count {
+            throw MemoryErrors.AddressOutOfRange
+        } else {
+            poke(address, bytes: data)
+            
+            // add this range to our ROM's table
+            rom_ranges.append((start: address, end: address + data.count - 1))
+        }
+    }
+    
+    func getRomRanges() -> RomRanges {
+        return rom_ranges
     }
     
     private func isAddressReadOnly(address: Int) -> Bool {
@@ -49,19 +87,5 @@ class Memory {
         }
         
         return false
-    }
-    
-    func poke(address: Int, bytes: [UInt8]) {
-        var my_address = address
-        if !isAddressReadOnly(address) {
-            for byte in bytes {
-                data[my_address++] = byte
-            }
-            
-        }
-    }
-    
-    func peek(address: Int) -> UInt8 {
-        return data[address]
     }
 }
