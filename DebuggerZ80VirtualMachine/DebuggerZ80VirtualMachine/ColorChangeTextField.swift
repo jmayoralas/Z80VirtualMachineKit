680eec4c740b6bd6be619276c07fb1de80327065
Jose Luis Fernández-Mayoralas <yo@jluis.me>
2016-01-08 17:46:56 +0100

    Versión inicial del debugger

---
+++ DebuggerZ80VirtualMachine/DebuggerZ80VirtualMachine/ColorChangeTextField.swift
@@ -0,0 +1,30 @@
+//
+//  ColorChangeTextFieldCell.swift
+//  DebuggerZ80VirtualMachine
+//
+//  Created by Jose Luis Fernandez-Mayoralas on 1/1/16.
+//  Copyright © 2016 lomocorp. All rights reserved.
+//
+
+import Cocoa
+
+class ColorChangeTextField : NSTextField {
+    var originalTextColor : NSColor?
+    
+    override var stringValue: String {
+        get {
+            return super.stringValue
+        }
+        set {
+            if super.stringValue != newValue {
+                if originalTextColor == nil {
+                    originalTextColor = super.textColor
+                }
+                super.textColor = NSColor.redColor()
+            } else {
+                super.textColor = originalTextColor
+            }
+            super.stringValue = newValue
+        }
+    }
+}
---
+++ DebuggerZ80VirtualMachine/Z80VirtualMachineKit.framework/Versions/A/Headers/Z80VirtualMachineKit.h
@@ -0,0 +1,19 @@
+//
+//  Z80VirtualMachineKit.h
+//  Z80VirtualMachineKit
+//
+//  Created by Jose Luis Fernandez-Mayoralas on 31/12/15.
+//  Copyright © 2015 lomocorp. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Z80VirtualMachineKit.
+FOUNDATION_EXPORT double Z80VirtualMachineKitVersionNumber;
+
+//! Project version string for Z80VirtualMachineKit.
+FOUNDATION_EXPORT const unsigned char Z80VirtualMachineKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Z80VirtualMachineKit/PublicHeader.h>
+
+
