f85820b7ea72ae86989b2040db37628396b3f99e
Jose Luis Fernández-Mayoralas <yo@jluis.me>
2016-05-01 19:56:02 +0200

    Aquí defino una clase Bus que extiende a BusComponent con métodos para añadir y borrar componentes del bus, manteniendo una lista de componentes "conectados" a ella.
    El método borrar me temo que no va a funcionar, hasta que vea cómo defino la igualdad entre dos BusComponent. Ahora lo hago con === lo cual no va a funcionar ya que eso símplemente compara si dos variables referencia a la misma instancia, que no será el caso.
    
    También defino un caso particular de Bus de 16 bits con un espacio de direccionamiento de 64k que será en el que basaré los buses del Z80. Esta clase, lo que hace es mantener una lista de 64 posiciones en la que almacena en cada posición el BusComponent que se encarga de gestionar ese kbyte del bus, preguntándole al propio componente su dirección base y su tamaño dentro del bus.
    
    Sobreescribe también los métodos write y read para realizar una búsqueda en este array de 64 posiciones haciendo que sea el componente que corresponda a la dirección solicitada el que responda a cada método.

---
+++ Z80VirtualMachineKit/Z80VirtualMachineKit/Bus.swift
@@ -0,0 +1,68 @@
+//
+//  Bus.swift
+//  Z80VirtualMachineKit
+//
+//  Created by Jose Luis Fernandez-Mayoralas on 1/5/16.
+//  Copyright © 2016 lomocorp. All rights reserved.
+//
+
+import Foundation
+
+class Bus : BusComponent {
+    private var bus_components = [BusComponent]()
+    
+    func addBusComponent(bus_component: BusComponent) {
+        bus_components.append(bus_component)
+        onComponentsUpdated()
+    }
+    
+    func deleteBusComponent(bus_component: BusComponent) {
+        for (index, component) in bus_components.enumerate() {
+            if component === bus_component {
+                bus_components.removeAtIndex(index)
+                onComponentsUpdated()
+                return
+            }
+        }
+    }
+    
+    func onComponentsUpdated() {
+        
+    }
+}
+
+class Bus16 : Bus {
+    private var paged_components : [BusComponent]
+    
+    init() {
+        let dummy_component = BusComponent(base_address: 0x0000, block_size: 0x0000)
+        paged_components = Array(count: 64, repeatedValue: dummy_component)
+        
+        super.init(base_address: 0x0000, block_size: 0x10000)
+    }
+    
+    override func onComponentsUpdated() {
+        for component in bus_components {
+            let start = Int(component.getBaseAddress() / 1024)
+            let end = start + component.getBlockSize() / 1024
+            
+            for i in start...end {
+                paged_components[i] = component
+            }
+        }
+    }
+    
+    override func write(address: UInt16, value: UInt8) {
+        let index_component = (Int(address) & 0xFFFF) / 1024
+        
+        if index_component < paged_components.count {
+            paged_components[index_component].write(address, value: value)
+        }
+    }
+    
+    override func read(address: UInt16) -> UInt8 {
+        let index_component = (Int(address) & 0xFFFF) / 1024
+        
+        return (index_component < paged_components.count) ? paged_components[index_component].read(address) : 0xFF
+    }
+}
