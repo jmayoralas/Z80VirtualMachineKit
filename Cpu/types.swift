e54c604c978e94f632cd0037a9fc893ffe70c744
Jose Luis Fernández-Mayoralas <yo@jluis.me>
2016-05-02 12:31:05 +0200

    Defino los registros de la cpu como versiones almacenadas de los de 8 bits y versiones calculadas de los de 16 bits.

--- Cpu/types.swift
+++ Cpu/types.swift
@@ -44,19 +44,6 @@ public struct Registers {
     public var e: UInt8 = 0
     public var l: UInt8 = 0
     
-    // Alternate Register Set
-    // accumulator
-    public var a_: UInt8 = 0
-    public var b_: UInt8 = 0
-    public var d_: UInt8 = 0
-    public var h_: UInt8 = 0
-    
-    // flags
-    public var f_: UInt8 = 0
-    public var c_: UInt8 = 0
-    public var e_: UInt8 = 0
-    public var l_: UInt8 = 0
-    
     // Interrupt Vector
     public var i: UInt8 = 0
     
@@ -68,6 +55,76 @@ public struct Registers {
     public var ixl: UInt8 = 0
     public var iyh: UInt8 = 0
     public var iyl: UInt8 = 0
+
+    // 16 bit registers
+    // primary
+    var af: UInt16 {
+        get {
+            return UInt16(Int(Int(self.a) * 0x100) + Int(self.f))
+        }
+        set(newValue) {
+            self.a = newValue.high
+            self.f = newValue.low
+        }
+    }
+
+    var bc: UInt16 {
+        get {
+            return UInt16(Int(Int(self.b) * 0x100) + Int(self.c))
+        }
+        set(newValue) {
+            self.b = newValue.high
+            self.c = newValue.low
+        }
+    }
+
+    var hl: UInt16 {
+        get {
+            return UInt16(Int(Int(self.h) * 0x100) + Int(self.l))
+        }
+        set(newValue) {
+            self.h = newValue.high
+            self.l = newValue.low
+        }
+    }
+
+    var de: UInt16 {
+        get {
+            return UInt16(Int(Int(self.d) * 0x100) + Int(self.e))
+        }
+        set(newValue) {
+            self.d = newValue.high
+            self.e = newValue.low
+        }
+    }
+    
+    // Alternate Register Set
+    // accumulator
+    public var af_: UInt16 = 0
+    public var bc_: UInt16 = 0
+    public var de_: UInt16 = 0
+    public var hl_: UInt16 = 0
+    
+    // index
+    var ix: UInt16 {
+        get {
+            return UInt16(Int(Int(self.ixh) * 0x100) + Int(self.ixl))
+        }
+        set(newValue) {
+            self.ixh = newValue.high
+            self.ixl = newValue.low
+        }
+    }
+    
+    var iy: UInt16 {
+        get {
+            return UInt16(Int(Int(self.iyh) * 0x100) + Int(self.iyl))
+        }
+        set(newValue) {
+            self.iyh = newValue.high
+            self.iyl = newValue.low
+        }
+    }
     
     // Stack Pointer
     public var sp: UInt16 = 0
