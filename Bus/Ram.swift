ed4f52cb410c9b1345099b07369de797200ca824
Jose Luis Fernandez-Mayoralas <yo@jluis.me>
2016-05-04 17:30:09 +0200

    Agrego a la clase Ram la posibilidad de informar a un delegado cuando se produzcan escrituras en su buffer.
    También personalizo el método dumpFromAddres para que devuelve un volcado de su buffer.

--- Bus/Ram.swift
+++ Bus/Ram.swift
@@ -11,6 +11,8 @@ import Foundation
 class Ram : BusComponent {
     var buffer : [UInt8]
     
+    var delegate : MemoryChange?
+    
     override init(base_address: UInt16, block_size: Int) {
         buffer = Array(count: block_size, repeatedValue: 0x00)
         
@@ -23,5 +25,16 @@ class Ram : BusComponent {
     
     override func write(address: UInt16, value: UInt8) {
         buffer[Int(address) - Int(self.base_address)] = value
+        delegate?.MemoryWriteAtAddress?(Int(address), byte: value)
+    }
+    
+    override func dumpFromAddress(fromAddress: Int, count: Int) -> [UInt8] {
+        let topAddress = Int(self.base_address) + self.block_size - 1
+        
+        let myFromAddress = (fromAddress < 0 ? 0 : fromAddress) - Int(self.base_address)
+        var myToAddress = fromAddress + (count > buffer.count ? buffer.count : count) - 1 - Int(self.base_address)
+        myToAddress = myToAddress > topAddress ? topAddress : myToAddress
+        
+        return Array(buffer[myFromAddress...myToAddress])
     }
 }
