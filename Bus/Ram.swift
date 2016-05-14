217ceb98807aed092a8331f00ccbbc85129306f2
Jose Luis Fernández-Mayoralas <yo@jluis.me>
2016-05-14 21:10:12 +0200

    Definimos un nuevo protocolo MemoryProtocol para encapsular ahí los requerimientos de cualquier cosa que quiera actuar como memoria del sistema y completar con la función dumpFromAddress.
    Cambiamos la clase Ram para que cumpla con el protocolo MemoryProtocol.
    
    De todos modos, aún no estoy convencido de que todo este lío que he montado con los protocolos sea mejor que usar herencia de clases directamente desde BusComponent. Me ahorraría tener que definir las propiedades block_size y base_address en la clase Ram, ya que estarían gestionadas directamente por BusComponent

--- Bus/Ram.swift
+++ Bus/Ram.swift
@@ -8,27 +8,13 @@
 
 import Foundation
 
-class Ram : BusComponent {
-    var buffer : [UInt8]
-    
-    var delegate : MemoryChange?
-    
-    override init(base_address: UInt16, block_size: Int) {
-        buffer = Array(count: block_size, repeatedValue: 0x00)
-        
-        super.init(base_address: base_address, block_size: block_size)
-    }
-    
-    override func read(address: UInt16) -> UInt8 {
-        return buffer[Int(address) - Int(self.base_address)]
-    }
-    
-    override func write(address: UInt16, value: UInt8) {
-        buffer[Int(address) - Int(self.base_address)] = value
-        delegate?.MemoryWriteAtAddress?(Int(address), byte: value)
-    }
-    
-    override func dumpFromAddress(fromAddress: Int, count: Int) -> [UInt8] {
+protocol MemoryProtocol : class, BusComponentBase {
+    var buffer: [UInt8] { get set }
+    var delegate : MemoryChange? { get set }
+}
+
+extension MemoryProtocol {
+    func dumpFromAddress(fromAddress: Int, count: Int) -> [UInt8] {
         let topAddress = Int(self.base_address) + self.block_size - 1
         
         let myFromAddress = (fromAddress < 0 ? 0 : fromAddress) - Int(self.base_address)
@@ -36,5 +22,27 @@ class Ram : BusComponent {
         myToAddress = myToAddress > topAddress ? topAddress : myToAddress
         
         return Array(buffer[myFromAddress...myToAddress])
+    }
+}
+
+class Ram : MemoryProtocol {
+    var buffer : [UInt8]
+    var delegate : MemoryChange?
+    var base_address: UInt16
+    var block_size: Int
+    
+    init(base_address: UInt16, block_size: Int) {
+        self.base_address = base_address
+        self.block_size = block_size
+        buffer = Array(count: block_size, repeatedValue: 0x00)
+    }
+    
+    func write(address: UInt16, value: UInt8) {
+        buffer[Int(address) - Int(self.base_address)] = value
+        delegate?.MemoryWriteAtAddress?(Int(address), byte: value)
+    }
+    
+    func read(address: UInt16) -> UInt8 {
+        return buffer[Int(address) - Int(base_address)]
     }
 }
