a00283918a80f23ab01fe461a72c935eba323766
Jose Luis Fernández-Mayoralas <yo@jluis.me>
2016-05-15 18:32:34 +0200

    Nueva aproximación para la ULA.
    Me he definido un protocolo UlaDelegate con los tres métodos que la ULA tiene que capturar, memoryWrite, ioWrite e ioRead.
    
    La nueva clase Ula se adhiere a este protocolo UlaDelegate y define dos propiedades. Una almacenará la memoria (ULAMemory) y la otra la parte de IO (ULAIO).
    
    Las clases ULAMemory incorporan una nueva propiedad ulaDelegate, que será llamada desde cada método de lectura y escritura. Así, si la cpu hace un ioBus.read, estará llamando al read de la parte IO de la ULA, y esta llamará al ioRead del delegado.
    
    La instancia de Ula podrá acceder a la memoria, usando la propiedad memory y a la parte IO usando su propiedad io.
    
    Desconozco la eficiencia que tendrá esto, pero por ahora es lo mejor que se me ha ocurrido para tener una instancia Ula que acceda tanto a la memoria como a la io.

--- Bus/Ula.swift
+++ Bus/Ula.swift
@@ -8,32 +8,55 @@
 
 import Foundation
 
+protocol UlaDelegate {
+    func memoryWrite(address: UInt16, value: UInt8)
+    func ioWrite(address: UInt16, value: UInt8)
+    func ioRead(address: UInt16) -> UInt8
+}
+
 class ULAMemory : Ram {
-    convenience init() {
-        self.init(base_address: 0x4000, block_size: 0x4000)
+    let ulaDelegate: UlaDelegate
+    
+    init(delegate: UlaDelegate) {
+        self.ulaDelegate = delegate
+        super.init(base_address: 0x4000, block_size: 0x4000)
     }
     
     override func write(address: UInt16, value: UInt8) {
         super.write(address, value: value)
-        memoryWrite(address, value: value)
-    }
-    
-    private func memoryWrite(address: UInt16, value: UInt8) {
-        NSLog("Writing to ULAMemory address: %@, value: %@", address.hexStr(), value.hexStr())
+        ulaDelegate.memoryWrite(address, value: value)
     }
 }
 
 class ULAIo : BusComponent {
-    convenience init() {
-        self.init(base_address: 0xFE, block_size: 1)
+    let ulaDelegate: UlaDelegate
+    
+    init(delegate: UlaDelegate) {
+        self.ulaDelegate = delegate
+        super.init(base_address: 0xFE, block_size: 1)
     }
     
     override func read(address: UInt16) -> UInt8 {
-        return ioRead(address)
+        return ulaDelegate.ioRead(address)
     }
     
     override func write(address: UInt16, value: UInt8) {
-        ioWrite(address, value: value)
+        ulaDelegate.ioWrite(address, value: value)
+    }
+}
+
+final class Ula: UlaDelegate {
+    var memory: ULAMemory!
+    var io: ULAIo!
+    
+    init() {
+        memory = ULAMemory(delegate: self)
+        io = ULAIo(delegate: self)
+    }
+    
+    func memoryWrite(address: UInt16, value: UInt8) {
+        NSLog("Writing to ULAMemory address: %@, value: %@", address.hexStr(), value.hexStr())
+        NSLog("in ula memory: %@", memory.read(address).hexStr())
     }
     
     func ioRead(address: UInt16) -> UInt8 {
@@ -44,9 +67,4 @@ class ULAIo : BusComponent {
     func ioWrite(address: UInt16, value: UInt8)  {
         NSLog("Writing to ULAIo address: %@, value: %@", address, value.hexStr())
     }
-}
-
-final class Ula {
-    let memory = ULAMemory()
-    let io = ULAIo()
 }
