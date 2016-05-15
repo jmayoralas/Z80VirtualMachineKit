cdc47daccb5f1e63deed81b57797393e0c60a672
Jose Luis Fernández-Mayoralas <yo@jluis.me>
2016-05-15 18:20:25 +0200

    Agrego una nueva clase Rom que es como una Ram pero que no hace nada al escribir en ella.

---
+++ Bus/Rom.swift
@@ -0,0 +1,15 @@
+//
+//  Rom.swift
+//  Z80VirtualMachineKit
+//
+//  Created by Jose Luis Fernandez-Mayoralas on 15/5/16.
+//  Copyright © 2016 lomocorp. All rights reserved.
+//
+
+import Foundation
+
+class Rom: Ram {
+    override func write(address: UInt16, value: UInt8) {
+        // do nothing
+    }
+}
