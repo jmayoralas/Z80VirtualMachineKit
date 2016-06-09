37dc78e29e28425c8a16761472da450d4df1ce61
Jose Luis Fernandez-Mayoralas <yo@jluis.me>
2016-06-09 17:14:17 +0200

    Muevo las clases ULAMemory y ULAIo ha sus propios fuentes y los quito del fuente de la Ula.

--- Ula/Ula.swift
+++ Ula/Ula.swift
@@ -27,37 +27,6 @@ protocol UlaDelegate {
     func ioRead(address: UInt16) -> UInt8
 }
 
-final class ULAMemory : Ram {
-    let ulaDelegate: UlaDelegate
-    
-    init(delegate: UlaDelegate) {
-        self.ulaDelegate = delegate
-        super.init(base_address: 0x4000, block_size: 0x4000)
-    }
-    
-    override func write(address: UInt16, value: UInt8) {
-        super.write(address, value: value)
-        ulaDelegate.memoryWrite(address, value: value)
-    }
-}
-
-final class ULAIo : BusComponent {
-    let ulaDelegate: UlaDelegate
-    
-    init(delegate: UlaDelegate) {
-        self.ulaDelegate = delegate
-        super.init(base_address: 0xFE, block_size: 1)
-    }
-    
-    override func read(address: UInt16) -> UInt8 {
-        return ulaDelegate.ioRead(address)
-    }
-    
-    override func write(address: UInt16, value: UInt8) {
-        ulaDelegate.ioWrite(address, value: value)
-    }
-}
-
 final class Ula: UlaDelegate {
     var memory: ULAMemory!
     var io: ULAIo!
