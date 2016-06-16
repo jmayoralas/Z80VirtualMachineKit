cb3269d62a36db5b55edd9b30404269f52b03f1f
Jose Luis Fernandez-Mayoralas <yo@jluis.me>
2016-06-16 21:48:58 +0200

    Screen border management now is completed.
    Blinking attribute is implemented too.

--- Ula/Ula.swift
+++ Ula/Ula.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+private let TICS_PER_LINE = 224
+private let SCREEN_LINES = 312 // 64 + 192 + 56
+private let TICS_PER_FRAME = TICS_PER_LINE * SCREEN_LINES
+private let WHITE_COLOR = PixelData(a: 255, r: 0xCD, g: 0xCD, b: 0xCD)
+
 protocol UlaDelegate {
     func onFrameCompleted()
 }
@@ -18,6 +23,11 @@ private struct PixelData {
     var g:UInt8
     var b:UInt8
 }
+
+extension PixelData: Equatable {}
+    private func ==(lhs: PixelData, rhs: PixelData) -> Bool {
+        return lhs.a == rhs.a && lhs.r == rhs.r && lhs.g == rhs.g && lhs.b == rhs.b
+    }
 
 private struct Attribute {
     var flashing: Bool
@@ -34,14 +44,12 @@ protocol InternalUlaOperationDelegate {
 final class Ula: InternalUlaOperationDelegate {
     var delegate: UlaDelegate?
     
-    private let TICS_PER_FRAME = 69888
-    
     var memory: ULAMemory!
     var io: ULAIo!
     
     private let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
     private let bitmapInfo:CGBitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedFirst.rawValue)
-    private var screen = [PixelData](repeating: PixelData(a: 255, r: 0xCD, g: 0xCD, b: 0xCD), count: 320 * 240)
+    private var screen = [PixelData](repeating: WHITE_COLOR, count: 320 * 240)
     private let colorTable = [
         PixelData(a: 255, r: 0, g: 0, b: 0),
         PixelData(a: 255, r: 0, g: 0, b: 0xCD),
@@ -62,7 +70,14 @@ final class Ula: InternalUlaOperationDelegate {
         PixelData(a: 255, r: 0xFF, g: 0xFF, b: 0xFF),
     ]
     
+    private var borderColor: PixelData = WHITE_COLOR
+    
     private var frameTics: Int = 0
+    private var lineTics: Int = 0
+    private var screenLine: Int = 0
+    private var flashState: Bool = false
+    private var frames: Int = 0
+    private var screenDirty: Bool = false
     
     init() {
         memory = ULAMemory(delegate: self)
@@ -70,10 +85,11 @@ final class Ula: InternalUlaOperationDelegate {
     }
     
     func step(t_cycle: Int) {
+        lineTics += t_cycle
         frameTics += t_cycle
-        if frameTics >= TICS_PER_FRAME {
-            delegate?.onFrameCompleted()
-            frameTics -= TICS_PER_FRAME
+        
+        if lineTics > TICS_PER_LINE {
+            screenLineCompleted()
         }
     }
     
@@ -105,7 +121,8 @@ final class Ula: InternalUlaOperationDelegate {
     }
     
     func ioWrite(_ address: UInt16, value: UInt8)  {
-        NSLog("Writing to ULAIo address: %@, value: %@", address.hexStr(), value.hexStr())
+        // get the border color from value
+        borderColor = colorTable[Int(value) & 0x07]
     }
     
     func getScreen() -> NSImage {
@@ -125,13 +142,26 @@ final class Ula: InternalUlaOperationDelegate {
     }
     
     private func fillScreenEightBitLineAt(char x: Int, line y: Int, value: UInt8, attribute: Attribute) {
+        
+        let inkColor: PixelData!
+        let paperColor: PixelData!
+        
+        if flashState && attribute.flashing {
+            inkColor = attribute.paperColor
+            paperColor = attribute.inkColor
+        } else {
+            inkColor = attribute.inkColor
+            paperColor = attribute.paperColor
+        }
         let index = (y + 24) * 320 + x * 8 + 32
         var j = 0
         
         for i in (0...7).reversed() {
-            screen[index + j] = ((Int(value) & 1 << i) > 0) ? attribute.inkColor : attribute.paperColor
+            screen[index + j] = ((Int(value) & 1 << i) > 0) ? inkColor : paperColor
             j += 1
         }
+        
+        screenDirty = true
     }
     
     private func imageFromARGB32Bitmap(_ pixels:[PixelData], width:Int, height:Int) -> NSImage {
@@ -167,5 +197,55 @@ final class Ula: InternalUlaOperationDelegate {
             paperColor: colorTable[(value >> 3) & 0b00001111],
             inkColor: colorTable[((value >> 3) & 0b00001000) | (value & 0b00000111)]
         )
+    }
+    
+    private func screenLineCompleted() {
+        screenLine += 1
+        lineTics -= TICS_PER_LINE
+        
+        if 36 <= screenLine && screenLine <= 239 + 36 {
+            // the line is on the visible area of the screen
+            // update border color if we have to
+            let bitmapLine = screenLine - 36
+            let index = bitmapLine * 320
+            
+            // the bitmapLine background color has changed ?
+            if screen[index] != borderColor {
+                if 24 <= bitmapLine && bitmapLine < 24 + 192 {
+                    // bitmap border
+                    for i in 0..<32 {
+                        screen[index + i] = borderColor
+                    }
+                    for i in 256 + 32..<320 {
+                        screen[index + i] = borderColor
+                    }
+                } else {
+                    // above and below bitmap area border
+                    for i in 0..<320 {
+                        screen[index + i] = borderColor
+                    }
+                }
+            }
+        }
+        
+        if screenLine >= SCREEN_LINES {
+            frames += 1
+            if frames > 16 {
+                flashState = !flashState
+                updateScreenFlashing()
+                frames = 0
+            }
+            
+            delegate?.onFrameCompleted()
+            
+            frameTics -= TICS_PER_FRAME
+            screenLine = 0
+        }
+    }
+    
+    private func updateScreenFlashing() {
+        for i in 0..<0x300 {
+            updateCharAtOffset(i, attribute: getAttribute(Int(memory.read(0x5800 + UInt16(i)))))
+        }
     }
 }
