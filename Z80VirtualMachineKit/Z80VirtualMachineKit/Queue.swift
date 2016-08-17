694d52a85e38f9e905ce4f5d58f91daecc6faf59
Jose Luis Fernández-Mayoralas <yo@jluis.me>
2016-08-17 17:32:40 +0200

    Added count attribute and changed some methods visibility

--- Z80VirtualMachineKit/Z80VirtualMachineKit/Queue.swift
+++ Z80VirtualMachineKit/Z80VirtualMachineKit/Queue.swift
@@ -8,7 +8,7 @@
 
 
 // should be an inner class of Queue, but inner classes and generics crash the compiler, SourceKit (repeatedly) and occasionally XCode.
-class _QueueItem<T> {
+private class _QueueItem<T> {
     let value: T!
     var next: _QueueItem?
     
@@ -20,28 +20,38 @@ class _QueueItem<T> {
 ///
 /// A standard queue (FIFO - First In First Out). Supports simultaneous adding and removing, but only one item can be added at a time, and only one item can be removed at a time.
 ///
-public class Queue<T> {
+class Queue<T> {
     
     typealias Element = T
     
-    var _front: _QueueItem<Element>
-    var _back: _QueueItem<Element>
+    private var _front: _QueueItem<Element>
+    private var _back: _QueueItem<Element>
     
-    public init () {
+    private var _count: Int = 0
+    
+    var count: Int {
+        get {
+            return _count
+        }
+    }
+    
+    init () {
         // Insert dummy item. Will disappear when the first item is added.
         _back = _QueueItem(nil)
         _front = _back
     }
     
     /// Add a new item to the back of the queue.
-    public func enqueue (value: T) {
+    func enqueue (value: T) {
         _back.next = _QueueItem(value)
         _back = _back.next!
+        self._count += 1
     }
     
     /// Return and remove the item at the front of the queue.
-    public func dequeue () -> T? {
+    func dequeue () -> T? {
         if let newhead = _front.next {
+            self._count -= 1
             _front = newhead
             return newhead.value
         } else {
@@ -49,7 +59,8 @@ public class Queue<T> {
         }
     }
     
-    public func isEmpty() -> Bool {
+    func isEmpty() -> Bool {
         return _front === _back
     }
+    
 }
